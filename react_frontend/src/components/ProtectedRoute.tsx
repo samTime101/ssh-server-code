e26d6c0183950c5ab040918213bd8971a7519c09
adgator101 <aadityathapa977@gmail.com>
2025-09-20 09:13:09 +0545

    Remove unused components and services: Deleted Categories, RegForm, TeacherPanel, Questions, UserPanel, and related services to streamline the codebase.

--- react_frontend/src/components/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-const ProtectedRoute = () => {
-  const token = localStorage.getItem('accessToken');
-
-  return token ? <Outlet /> : <Navigate to="/" />;
-};
-
-export default ProtectedRoute;
