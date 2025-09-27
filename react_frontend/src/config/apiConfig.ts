d70c31004220e45ae9e2cec5e2b3b5bee62442de
adgator101 <aadityathapa977@gmail.com>
2025-09-28 00:08:43 +0545

    feat: enhance API configuration and implement axios instance with global logout handling

--- react_frontend/src/config/apiConfig.ts
+++ react_frontend/src/config/apiConfig.ts
@@ -1,6 +1,12 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export const API_ENDPOINTS = {
   login: `${API_BASE_URL}/signin/`,
   signup: `${API_BASE_URL}/signup/`,
+
+  createCategory: `${API_BASE_URL}/create/category/`,
+  getCategories: `${API_BASE_URL}/get/categories/`,
+
+  selectQuestions: `${API_BASE_URL}/select/questions/`,
+  attemptQuestion: `${API_BASE_URL}/user/attempt/`,
 };
