03d38a1e653040ad0cdf4e56b24c2961eafe4acd
adgator101 <aadityathapa977@gmail.com>
2025-10-09 23:37:43 +0545

    feat(auth): remove localStorage based user state and migrate to server based

--- react_frontend/src/config/apiConfig.ts
+++ react_frontend/src/config/apiConfig.ts
@@ -1,9 +1,12 @@
-export const API_BASE_URL = "http://localhost:8000/api"; //import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"; //"http://localhost:5173"
+// export const API_BASE_URL = "http://localhost:8000/api"; //import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"; //"http://localhost:5173"
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
 
 export const API_ENDPOINTS = {
   login: `${API_BASE_URL}/signin/`,
   signup: `${API_BASE_URL}/signup/`,
 
+  userInfo: `${API_BASE_URL}/user/`,
+  
   createCategory: `${API_BASE_URL}/create/category/`,
   getCategories: `${API_BASE_URL}/get/categories/`,
 
