5a1bb5de470b3372b19692795c6d9e7cee29b84d
arvind <sisani.tech@gmail.com>
2025-10-14 08:43:47 +0545

    Fixed the issue to get categories and sub categories question

--- django_backend/select_questions/serializers.py
+++ django_backend/select_questions/serializers.py
@@ -31,25 +31,25 @@ class QuestionResponseSerializer(serializers.Serializer):
 
 class SelectQuestionSerializer(serializers.Serializer):
 
-    categoryIds = serializers.ListField(child=serializers.IntegerField(), required=False, allow_empty=True)
-    subCategoryIds = serializers.ListField(child=serializers.IntegerField(), required=False, allow_empty=True)
-    subSubCategoryIds = serializers.ListField(child=serializers.IntegerField(), required=False, allow_empty=True)
+    categoryId = serializers.ListField(child=serializers.IntegerField(), required=False, allow_empty=True)
+    subCategoryId = serializers.ListField(child=serializers.IntegerField(), required=False, allow_empty=True)
+    subSubCategoryId = serializers.ListField(child=serializers.IntegerField(), required=False, allow_empty=True)
 
-    def validate_categoryIds(self, value):
+    def validate_categoryId(self, value):
         try:
             self.categoryNames = fetch_names(Category, value, 'categoryId', 'categoryName')
         except ObjectDoesNotExist as e:
             raise serializers.ValidationError(str(e))
         return value
 
-    def validate_subCategoryIds(self, value):
+    def validate_subCategoryId(self, value):
         try:
             self.subCategoryNames = fetch_names(SubCategory, value, 'subCategoryId', 'subCategoryName')
         except ObjectDoesNotExist as e:
             raise serializers.ValidationError(str(e))
         return value
 
-    def validate_subSubCategoryIds(self, value):
+    def validate_subSubCategoryId(self, value):
         try:
             self.subSubCategoryNames = fetch_names(SubSubCategory, value, 'subSubCategoryId', 'subSubCategoryName')
         except ObjectDoesNotExist as e:
