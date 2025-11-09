f787a211712087a9c4678d5bb25b7f7f191e30e7
adgator101 <aadityathapa977@gmail.com>
2025-11-09 11:32:58 +0545

    feat: update API endpoints for authentication and user profile, and add date utility functions

--- react_frontend/src/config/apiConfig.ts
+++ react_frontend/src/config/apiConfig.ts
@@ -1,19 +1,25 @@
 // export const API_BASE_URL = "http://localhost:8000/api"; //import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"; //"http://localhost:5173"
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
 
 export const API_ENDPOINTS = {
-  login: `${API_BASE_URL}/signin/`,
-  signup: `${API_BASE_URL}/signup/`,
+  login: `${API_BASE_URL}/auth/signin/`,
+  signup: `${API_BASE_URL}/auth/signup/`,
 
-  userInfo: `${API_BASE_URL}/user/`,
+  accountInfo: `${API_BASE_URL}/users/profile/`,
 
   createCategory: `${API_BASE_URL}/create/category/`,
-  getCategories: `${API_BASE_URL}/get/categories/`,
-
+  getCategories: `${API_BASE_URL}/categories/`,
+  getCategoriesWithHierarchy: `${API_BASE_URL}/questions/hierarchy/`,
   createSubCategory: `${API_BASE_URL}/create/subcategory/`,
   createSubSubCategory: `${API_BASE_URL}/create/subsubcategory/`,
 
   selectQuestions: `${API_BASE_URL}/select/questions/`,
   attemptQuestion: `${API_BASE_URL}/user/attempt/`,
   createQuestion: `${API_BASE_URL}/create/question/`,
+
+  // Admin Routes
+  usersList: `${API_BASE_URL}/users/`,
+  adminGetAllQuestions: `${API_BASE_URL}/questions`,
+  
 };
---
+++ react_frontend/src/utils/dateUtils.ts
@@ -0,0 +1,10 @@
+export const LOCALE = "en-US";
+
+export const DATE_OPTIONS = {
+  year: "numeric",
+  month: "short",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: true,
+};
