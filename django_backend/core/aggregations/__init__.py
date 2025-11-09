bf59f54f18fe1a1b1c0d9d56660efda5a8e1e64b
samTime101 <srijanregmi44@gmail.com>
2025-11-09 15:10:38 +0545

    removed aggregation test files

--- django_backend/core/aggregations/__init__.py
+++
@@ -1,6 +0,0 @@
-from .module1 import *
-from .module2 import *
-from .module3 import *
-from .module4 import *
-from .module5 import *
-from .module6 import *
--- django_backend/core/aggregations/module2.py
+++
@@ -1,6 +0,0 @@
-# question count for subcategory
-add_question_count_field = {
-    "$addFields": {
-        "sub_category_question_count": {"$size": "$questions"}
-    }
-}
